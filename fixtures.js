526110b1f3ab737a13978eb5911cb55f9b265903
Patrick Weaver <patrick@booknooklearning.com>
2022-08-22 11:49:35 -0400

    001/create-fixtures

---
+++ fixtures.js
@@ -0,0 +1,28 @@
+const fixtureA1 = {
+    name: 'FA1',
+    type: 'square',
+    size: 4,
+    createdAt: new Date(),
+}
+
+const fixtureA2 = {
+    name: 'FA2',
+    type: 'square',
+    size: 3,
+    createdAt: new Date(),
+}
+
+const fixtureA3 = {
+    name: 'FA3',
+    type: 'square',
+    size: 15,
+    createdAt: new Date(),
+}
+
+
+
+module.export = {
+    fixtureA1,
+    fixtureA2,
+    fixtureA3,
+}
