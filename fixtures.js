044fdceb643e66b818ff07838da6d9bb379f0f05
Patrick Weaver <patrick@booknooklearning.com>
2022-08-22 11:50:58 -0400

    002-add-large-fixtures

--- fixtures.js
+++ fixtures.js
@@ -19,10 +19,34 @@ const fixtureA3 = {
     createdAt: new Date(),
 }
 
+const fixtureL1 = {
+    name: 'FL1',
+    type: 'square',
+    size: 500,
+    createdAt: new Date(),
+}
+
+const fixtureL2 = {
+    name: 'FL2',
+    type: 'square',
+    size: 1391,
+    createdAt: new Date(),
+}
+
+const fixtureL3 = {
+    name: 'FL3',
+    type: 'square',
+    size: 4818,
+    createdAt: new Date(),
+}
+
 
 
 module.export = {
     fixtureA1,
     fixtureA2,
     fixtureA3,
+    fixtureL1,
+    fixtureL2,
+    fixtureL3,
 }
